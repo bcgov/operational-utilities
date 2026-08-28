e2b9cc3d450c5588a15d3e80f9fbcde57e802b4d
Robert Xie <98110427+xieshihua@users.noreply.github.com>
2026-08-28 10:56:52 -0700

    Update temp_clean_up.cmd

--- cmd/temp_clean_up.cmd
+++ cmd/temp_clean_up.cmd
@@ -1,10 +1,18 @@
 echo off
 setlocal enabledelayedexpansion
 
-set BlockDivider0="*********************************************************************"
-REM set BlockDivider1="~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 set Name="temp_clean_up.cmd"
+
+set Block_Divider_0="*********************************************************************"
+REM set BlockDivider1="~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+rem Configue Info Sections
+set /A Max_Section_Items=6
+set Optional_Info_Sections=Usage,Example,Remark
+
+rem Set info sections
 set Purpose0="Remove files in [Path_to_Check] folder(s) and their sub folders that are older than [Cut_off_Days] days."
+
 rem Below are optional Help items. Delete or comment out any item that you do not use.
 rem use "." to insert a blank line.
 set Usage0="temp_clean_up.cmd [/?] [/T] [/D:Days] [/P:Path]"
@@ -14,21 +22,22 @@ set Usage3="  [/D:Days]  Optional, defaults to [Cut_off_Days]. Cut off days. e.g
 set Usage4="  [/P:Path]  Optional, defaults to [Path_to_Check]. Folder to be scanned and cleaned up. e.g. /P:C:\temp."
 set Usage5="  [/t | /T]  Optional, defaults to FALSE. Lists files meeting the criteria without deleting them."
 set Usage6="."
+
 set Example0="The following command scans [C:\Program Files] and display the files that are older than 30 days."
 set Example1="."
 set Example2="  C:\dvlp>temp_clean_up.cmd /D:30 '/P:C:\Program Files' /t"
 set Example3="."
+
 set Remark0="* It goes through sub folders recursively."
 set Remark1="."
+
 rem set Reference0="- A thorough reference to Windows CMD commands can be found at: https://ss64.com/nt/"
 rem set Reference1="           - https://ss64.com/ also provides references of Linux, macOS, PowerShell, ASCII, VBScript, Tools, and Passwords."
-set Head_Sections=Usage,Example,Remark
-set Max_Help_Items=0,1,2,3,4,5,6
 
-echo %BlockDivider0:"=%
+echo %Block_Divider_0:"=%
 echo %Name:"=%:
 call :MSG_Lines "Purpose"
-echo %BlockDivider0:"=%
+echo %Block_Divider_0:"=%
 
 set /A Cut_off_Days=90
 
@@ -100,7 +109,7 @@ if not !Is_Help!==TRUE (
 
 if !Is_Help!==TRUE (
 	call :MSG_Help
-	exit /b 0
+	goto The_Exit
 )
 
 echo Cut_off_Days: !Cut_off_Days!
@@ -112,6 +121,8 @@ REM set /A Cut_off_Days=120
 REM set Path_to_Check="D:\tmp\apps_data","D:\tmp\web_temp","D:\tmp\sde_client"
 REM call :clean_up %Cut_off_Days%, %Path_to_Check%, %1
 
+rem the Exit point of the batch CMD.
+:The_Exit
 endlocal
 echo on
 exit /B %errorlevel%
@@ -139,14 +150,14 @@ Exit /B 0
 	echo.
 	REM echo %BlockDivider1:"=%
 	REM echo Name: %Name:"=% 
-	for %%a in (%Head_Sections%) do (
+	for %%a in (%Optional_Info_Sections%) do (
 		call :MSG_Lines "%%~a"
 	)
 	REM echo %BlockDivider1:"=%
 Exit /B 0
 
 :MSG_Lines
-	for %%i in (%Max_Help_Items%) do (
+	for /L %%i in (0,1,%Max_Section_Items%) do (
 		if defined %~1%%i (
 			set msg=!%~1%%i!
 			if !msg! equ "." (
