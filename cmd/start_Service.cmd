4251a235c329e730906aa7299d2004f77e949aa8
Robert Xie <98110427+xieshihua@users.noreply.github.com>
2026-08-28 10:58:19 -0700

    Update start_Service.cmd

--- cmd/start_Service.cmd
+++ cmd/start_Service.cmd
@@ -1,30 +1,39 @@
 echo off
 setlocal enabledelayedexpansion
 
-set BlockDivider0="*********************************************************************"
-REM set BlockDivider1="~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 set Name="start_service.cmd"
+
+set Block_Divider_0="*********************************************************************"
+REM set BlockDivider1="~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+rem Configue Info Sections
+set /A Max_Section_Items=5
+set Optional_Info_Sections=Usage,Example
+
+rem Set info sections
 set Purpose0="Start a Windows service if it is not running."
+
 set Usage0="start_service.cmd [/?] [/T] service_name"
 set Usage1="."
 set Usage2="  [/?]          Optional. Display the Help info, defaults to FALSE."
 set Usage3="  [/T]          Optional. Test run or dry run, defaults to FALSE."
 set Usage4="  service_name  The name of a Windows service."
 set Usage5="."
+
 set Example0="The following command shows the status of AppReadiness."
 set Example1="If the service is not running, it shows the command to start the service:"
 set Example2="."
 set Example3="  C:\dvlp>start_service.cmd AppReadiness /t"
 set Example4="."
-rem set Remark="Set the EffArg_Required to the number of mandatory arguments."
-rem set Reference="A thorough reference to Windows CMD commends: https://ss64.com/nt/"
-set Head_Sections=Usage,Example
-set Max_Help_Items=0,1,2,3,4,5
 
-echo %BlockDivider0:"=%
+rem set Remark0="Set the EffArg_Required to the number of mandatory arguments."
+
+rem set Reference0="A thorough reference to Windows CMD commends: https://ss64.com/nt/"
+
+echo %Block_Divider_0:"=%
 echo %Name:"=%:
 call :MSG_Lines "Purpose"
-echo %BlockDivider0:"=%
+echo %Block_Divider_0:"=%
 
 REM 'shift' will process all the arguments.
 set /A Arg_Count=0
@@ -84,7 +93,7 @@ if not !Is_Help!==TRUE (
 
 if !Is_Help!==TRUE (
 	call :MSG_Help
-	exit /b 0
+	goto The_Exit
 )
 
 if !Is_Test!==TRUE (
@@ -113,6 +122,10 @@ if !Is_Test!==TRUE (
 	echo.
 )
 
+rem the Exit point of the batch CMD.
+:The_Exit
+endlocal
+echo on
 Exit /B 0
 
 :MSG_EffectiveArgs
@@ -126,14 +139,14 @@ Exit /B 0
 	echo.
 	REM echo %BlockDivider1:"=%
 	REM echo Name: %Name:"=% 
-	for %%a in (%Head_Sections%) do (
+	for %%a in (%Optional_Info_Sections%) do (
 		call :MSG_Lines "%%~a"
 	)
 	REM echo %BlockDivider1:"=%
 Exit /B 0
 
 :MSG_Lines
-	for %%i in (%Max_Help_Items%) do (
+	for /L %%i in (0,1,%Max_Section_Items%) do (
 		if defined %~1%%i (
 			set msg=!%~1%%i!
 			if !msg! equ "." (
